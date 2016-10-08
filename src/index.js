b366110cc11ecdf12ac6c518c34beb4de354b4a9
pwentz <patrick.w.wentz@gmail.com>
2016-10-08 16:36:15 -0600

    close #20 add custom map events for markers/venues, implement additional reducer for logging browser history throughout application, sync foursquare venue data to marker component.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-router-redux'
 import App from './App.js.jsx';
 import MapContainer from './components/MapContainer.js.jsx'
 import HomeContainer from './components/HomeContainer.js.jsx'
+import VenueDataContainer from './components/VenueDataContainer.js.jsx'
 import './index.css';
 import reducer from './reducer'
-const store = createStore(reducer)
+
+const middleware = routerMiddleware(browserHistory)
+const reducers = combineReducers({reducer, routing: routerReducer})
+const store = createStore(
+  reducers,
+  applyMiddleware(middleware)
+)
+
+const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
   <Provider store={ store }>
-    <Router history={ browserHistory }>
+    <Router history={ history }>
 
       <Route path="/" component={App}>
 
         <IndexRoute component={HomeContainer}/>
 
-        <Route path="/venues" component={MapContainer}/>
+        <Route path="/venues" component={MapContainer}>
+          <Route path="/venues/:venueId" component={VenueDataContainer} />
+        </Route>
 
       </Route>
 
