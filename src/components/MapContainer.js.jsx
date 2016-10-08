00e42ea7be9545e01bd99ea1fdb74004edae3837
pwentz <patrick.w.wentz@gmail.com>
2016-10-08 11:13:57 -0600

    close #23 add custom marker icons for venues.

--- src/components/MapContainer.js.jsx
+++ src/components/MapContainer.js.jsx
@@ -7,7 +7,7 @@ class MapContainer extends Component {
   componentDidMount() {
     const lat = this.props.userLocation.lat
     const lng = this.props.userLocation.lng
-    const myMap = L.map('g-map').setView([lat, lng], 15)
+    const myMap = L.map('g-map').setView([lat, lng], 16)
     L.tileLayer(`https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=${process.env.mapbox_access_token}`, {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>'
     }).addTo(myMap);
@@ -16,7 +16,11 @@ class MapContainer extends Component {
 
   addMarkers(myMap) {
     this.props.venues.forEach( v => {
-      const marker = L.marker([v.location.lat, v.location.lng]).addTo(myMap)
+      const divIcon = L.icon({
+        iconSize: [50,50],
+        iconUrl: 'https://cdn2.iconfinder.com/data/icons/location-map-simplicity/512/pub_beer-256.png',
+      })
+      const marker = L.marker([v.location.lat, v.location.lng], {icon: divIcon}).addTo(myMap)
       marker.bindPopup(v.name)
     })
   }
